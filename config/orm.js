365199a75ecb1fa47b53561352b3835c7db2298b
jleilani <jxleilani@gmail.com>
2020-11-01 18:30:04 -0500

    create orm

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,29 @@
 const connection = require('../config/connection.js');
 
-// selectAll()
-// insertOne()
-// updateOne()
+//Do I need the print question marks stuff???
 
-module.exports = ORM;
+var orm = {
+    selectAll: function(table, callback){
+        var queryString = "SELECT * FROM ??;"
+        connection.query(queryString, [table], function(err, result){
+            if (err) throw err;
+            callback(result);
+        });
+    },
+    insertOne: function(table, columns, values, callback){
+        var queryString = "INSERT INTO ?? (??) VALUES (?);"
+        connection.query(queryString, [table, columns, values], function(err,result){
+            if (err) throw err;
+            callback(result);
+        });
+    },
+    updateOne: function(table, columns, values, callback){
+        var queryString = "UPDATE ?? SET ?? WHERE ?";
+        connection.query(queryString, [table, columns, values], function(err,result){
+            if (err) throw err;
+            callback(result);
+        });
+    }
+};
+
+module.exports = orm;
