9143f049b1308cae5d72eb4c0cbaf1ecb732a55d
jleilani <jxleilani@gmail.com>
2020-11-02 11:33:46 -0500

    set up server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+//Serve static content from public directory
+app.use(express.static("public"));
+//Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Use express-handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Giver server access to routes
+var routes = require("./controllers/burger_controller.js");
+app.use(routes);
+
+//Server listening
+app.listen(PORT, function(){
+    console.log('Server listening on: http://localhost:' + PORT);
+});
